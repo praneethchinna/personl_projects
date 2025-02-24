81032b33ed5683a6b8c60165907ff5a5c990c8c6
praneethm <praneeth.mudhigonda_consultant@jukshio.com>
2025-02-24 11:24:21 +0530

    reverted changes for splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,8 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
-import 'package:gif_view/gif_view.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:ysr_project/colors/app_colors.dart';
 import 'package:ysr_project/features/home_screen/ui/home_tab_screen.dart';
 import 'package:ysr_project/features/login/ui/login_screen.dart';
 import 'package:ysr_project/services/user/user_data.dart';
@@ -42,7 +40,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         scaffoldBackgroundColor:
             Colors.grey.shade200, // Background color for Scaffold
@@ -70,65 +67,82 @@ class SplashScreen extends ConsumerStatefulWidget {
 
 class _SplashScreenState extends ConsumerState<SplashScreen>
     with SingleTickerProviderStateMixin {
-  final GifController controller = GifController();
-  Timer? timer;
+  late AnimationController _animationController;
+
+  late Animation<double> _scaleAnimation;
 
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      controller.play();
-      // Stop 2 frames before the end using a timer
-      timer = Timer(const Duration(milliseconds: 1000), () async {
-        controller.pause();
-        await Future.delayed(Duration(seconds: 2));
-        SharedPreferences prefs = await SharedPreferences.getInstance();
 
-        String? userData = prefs.getString("userData");
+    // Create animation controller
+    _animationController = AnimationController(
+      duration: const Duration(seconds: 3),
+      vsync: this,
+    );
 
-        if (userData != null) {
-          final userRealData = jsonDecode(userData);
-          ref.read(userProvider.notifier).update(
-                (state) => User(
-                  message: userRealData['message'],
-                  userId: userRealData['user_id'],
-                  name: userRealData['name'],
-                  role: userRealData['role'],
-                  mobile: userRealData['mobile'],
-                  parliament: userRealData['parliament'],
-                  constituency: userRealData['constituency'],
-                ),
-              );
-          Navigator.of(context).pushReplacement(
-            MaterialPageRoute(builder: (context) => HomeTabScreen()),
-          );
-        } else {
-          Navigator.of(context).pushReplacement(
-            MaterialPageRoute(builder: (context) => LoginScreen()),
-          );
-        }
-      });
+    // Create scale animation
+    //use elasticout for better effect
+    _scaleAnimation = Tween<double>(begin: 0.1, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.elasticInOut
+      ),
+    );
+
+    // Start the animation
+    _animationController.forward();
+
+    // Navigate to the main screen after animation completes
+    Future.delayed(const Duration(seconds: 4), () async {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+
+      String? userData = prefs.getString("userData");
+
+      if (userData != null) {
+        final userRealData = jsonDecode(userData);
+        ref.read(userProvider.notifier).update(
+              (state) => User(
+                message: userRealData['message'],
+                userId: userRealData['user_id'],
+                name: userRealData['name'],
+                role: userRealData['role'],
+                mobile: userRealData['mobile'],
+                parliament: userRealData['parliament'],
+                constituency: userRealData['constituency'],
+              ),
+            );
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => HomeTabScreen()),
+        );
+      } else {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => LoginScreen()),
+        );
+      }
     });
   }
 
   @override
   void dispose() {
-    controller.dispose();
-    timer?.cancel();
+    _animationController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: AppColors.mySoftPearl,
-      child: GifView.asset(
-        'assets/speech (1).gif',
-        height: 200,
-        width: 200,
-        frameRate: 30,
-        controller: controller,
-        loop: false,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ScaleTransition(
+          scale: _scaleAnimation,
+          child: Image.asset(
+            'assets/ysr.png',
+            width: 700,
+            height: 700,
+            fit: BoxFit.contain,
+          ),
+        ),
       ),
     );
   }
